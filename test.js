b2d226bd7b2882628011d15430bedf777508fc4f
naeem252 <naeemhasan252@gmail.com>
2022-10-15 13:14:35 +0600

    update

--- test.js
+++ test.js
@@ -78,46 +78,6 @@ class MyPushConnector extends SEPCPushConnector {
     return this.lastChangeBatchUuid;
   }
 }
-class MyPullConnector extends SEPCPullConnector {
-  // constructor(url, port) {
-  //   super(url, port);
-  // }
-  // variable where the last update batch uuid will be saved; use your own logic to save the latest processed update
-  lastChangeBatchUuid = null;
-
-  // override method responsible for notifying
-  // about new initial data messages
-  notifyInitialDump(initialData) {
-    //console.log("data", initialData);
-    console.log(
-      "initialData =-=-=-=-=-=",
-      initialData.batchId,
-      "batchleft",
-      initialData.batchesLeft
-    );
-    // const entity = initialData.entities[0].entityClass;
-    // var Entity = mongoose.model(entity, entitySchema);
-    // console.log(entity);
-    // Entity.insertMany(initialData.entities)
-    //   .then(function () {
-    //     console.log("Data inserted"); // Success
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error); // Failure
-    //   });
-  }
-
-  // override method responsible for notifying
-  // about new update messages
-  notifyEntityUpdates(updateData) {
-    console.log("data update");
-  }
-
-  // return the last saved uuid as you see fit, in order to avoid re-subscription
-  getLastAppliedEntityChangeBatchUuid() {
-    return this.lastChangeBatchUuid;
-  }
-}
 // const thing = new Thing({ iAmNotInTheSchema: true });
 // thing.save(); // iAmNotInTheSchema is not saved to the db
 
@@ -126,24 +86,18 @@ class MyPullConnector extends SEPCPullConnector {
 // NOTE: for SEPCPushConnector protocol must be absent
 
 // replace host and port with proper ones
-// const pushConnector = new MyPushConnector("sept.oddsmatrix.com", 7000);
-const pullConnector = new MyPullConnector("sept.oddsmatrix.com", 7000);
+const pushConnector = new MyPushConnector("sept.oddsmatrix.com", 7000);
 
 // provide subscription name
 // replace with proper subscriptionName
-// pushConnector.start("LawleyandAllen");
-pullConnector.start("LawleyandAllen");
+pushConnector.start("LawleyandAllen");
 
-// pushListenTo(Events.runtimeError, (error) => {
-//   // process different errors that could occur
-// });
-pullListenTo(Events.runtimeError, (error) => {
+pushListenTo(Events.runtimeError, (error) => {
   // process different errors that could occur
 });
 
 // call stop() to close the connection and to avoid memory leaks
-// onProcessExit(pushConnector.stop);
-onProcessExit(pullConnector.stop);
+onProcessExit(pushConnector.stop);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
